a1b5195b1cb3bbd6711dba4d386a2a60e2e6dbda
sh4d0wy <sakshambhugra8@gmail.com>
2026-01-24 10:41:30 +0530

    chore: fixed build issues and pm2 support

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -3,12 +3,11 @@ import { createUserSchema } from "../schema/createUserSchema";
 import type { Request, Response } from "express";
 import { sendVerificationEmail } from "../utils/email";
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { ethers } from "ethers";
 import { deleteUserSchema } from "../schema/deleteUserSchema";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(process.cwd());
+// const __dirname = path.dirname(__filename);
 
 export default{
     getUserDetails:async(req: Request, res: Response) => {
